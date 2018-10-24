23f135d7cdac5a4ed40de51b1f1dd05d199726d0
Christopher Fields <chrisfields@coreconstruction.com>
2018-10-24 09:25:40 -0500

    edited paths to create-react-app from server.

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use('/api/thoughts/', require('./server/routes/thoughts-route'));
 
 app.get('/', (req, res) => {
-  res.sendFile('index.html', { root: __dirname + '/client/build/' });
+  res.sendFile('index.html', { root: __dirname + '/client/mern_azure_example/build/' });
 });
 
 const { PORT } = process.env;
